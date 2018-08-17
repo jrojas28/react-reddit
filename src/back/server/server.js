829b742fb70b1de9ccda26d29769bffb588dd3c7
Luis Rojas <lrojas94@gmail.com>
2018-08-17 19:05:31 -0400

    Ensure backend properly renders base js

--- src/back/server/server.js
+++ src/back/server/server.js
@@ -6,12 +6,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import getLogger from '../util/log';
+import frontEndRouter from '../routes/front-end';
 
 const app = express();
 const log = getLogger('server');
 
 app.use(bodyParser.json());
+app.use(express.static('./'));
 
+/**
+ * Routers:
+ */
+app.use(frontEndRouter);
+
+/**
+ * health end-points
+ */
 app.get('/health-check', (req, res) => res.json({ healthy: true }));
 
 export default () => {
