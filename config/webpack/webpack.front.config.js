678bf78c7745e1b50eabc8ecc87e4b0347660c87
Luis Rojas <lrojas94@gmail.com>
2018-08-16 23:09:29 -0400

    Re-organize folder structure

--- config/webpack/webpack.front.config.js
+++ config/webpack/webpack.front.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, '../../src/front/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../../dist'),
     publicPath: 'dist',
     filename: 'reddit.bundle.js',
   },
--- src/front/index.js
+++ src/front/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import App from './app/components/app/App';
 
 require('./styles/styles.scss');
 
