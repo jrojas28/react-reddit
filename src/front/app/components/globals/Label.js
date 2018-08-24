96d845a4e29c5af8f9ff67c36b62284a6c4da5a7
Rafael Brache <rbrache@Rafaels-MacBook-Pro.local>
2018-08-23 22:05:48 -0400

    implementing required changes

--- src/front/app/components/globals/Label.js
+++ src/front/app/components/globals/Label.js
@@ -1,6 +1,6 @@
-import classNames from 'classnames';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 class Label extends Component {
    static propTypes = {
@@ -11,17 +11,19 @@ class Label extends Component {
 
   render() {
     const {
-     className,
-     children,
-     ...props,
+      htmlFor,
+      className,
+      children,
+      ...props
     } = this.props;
 
     const labelClasses = classNames({
+    'form-control-label': true,
     [className]: !!className,
     });
 
     return (
-      <label {...props} className={labelClasses}>
+      <label htmlFor={htmlFor} {...props} className={labelClasses}>
         {children}
       </label>
     );
