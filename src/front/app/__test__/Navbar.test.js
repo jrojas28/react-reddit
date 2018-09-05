cd6bc9080725faf7718725d17faf579fec972951
Jaime Rojas <j_rojas94@hotmail.com>
2018-09-04 23:09:25 -0400

    Add toggler to Nabvar

--- src/front/app/__test__/Navbar.test.js
+++ src/front/app/__test__/Navbar.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
+
+import Button from '../components/globals/Button';
 import Navbar from '../components/globals/Navbar';
 
 describe('(Component) Navbar', () => {
@@ -41,5 +43,58 @@ describe('(Component) Navbar', () => {
     );
 
     expect(wrapper.attr('class')).toBe('navbar navbar-dark my-class');
+  });
+
+  it('Should render a toggler button when useToggler is set', () => {
+    const wrapper = shallow(
+      <Navbar className="my-class" useToggler>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </Navbar>
+    );
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+
+  it('Should render a custom toggler when toggler is sent', () => {
+    const wrapper = shallow(
+      <Navbar
+        className="my-class"
+        useToggler
+        toggler={
+          ({ onClick }) => (
+            <div className="my-div" onClick={onClick} role="presentation">
+              Hello
+            </div>
+          )
+        }
+      >
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </Navbar>
+    );
+    expect(wrapper.find('div.my-div')).toHaveLength(1);
+  });
+
+  it('Should render a brand when sent', () => {
+    const wrapper = shallow(
+      <Navbar
+        className="my-class"
+        useToggler
+        brand={
+          () => (
+            <div className="my-div">
+              Hello
+            </div>
+          )
+        }
+      >
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </Navbar>
+    );
+    expect(wrapper.find('div.my-div')).toHaveLength(1);
   });
 });
--- src/front/app/components/globals/Navbar.js
+++ src/front/app/components/globals/Navbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import Button from './Button';
+
 const colorSchemes = [
   'light',
   'dark',
@@ -16,10 +18,41 @@ class Navbar extends Component {
     ]).isRequired,
     className: PropTypes.string,
     colorScheme: PropTypes.oneOf(colorSchemes),
+    useToggler: PropTypes.bool,
+    toggler: PropTypes.func,
+    brand: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.instanceOf(Component),
+      PropTypes.func,
+    ]),
   };
 
   static defaultProps = {
     colorScheme: 'dark',
+    toggler: ({
+      onClick,
+      isOpen,
+    }) => {
+      const togglerClass = classNames({
+        'navbar-toggler': true,
+        collapsed: !isOpen,
+      });
+      return (
+        <Button className={togglerClass} buttonType="dark" onClick={onClick}>
+          <span className="navbar-toggler-icon" />
+        </Button>
+      );
+    },
+  };
+
+  state = {
+    isOpen: false,
+  };
+
+  handleTogglerClick = () => {
+    this.setState(({ isOpen }) => ({
+      isOpen: !isOpen,
+    }));
   };
 
   render() {
@@ -27,14 +60,38 @@ class Navbar extends Component {
       children,
       className,
       colorScheme,
+      useToggler,
+      toggler,
+      brand,
       ...props
     } = this.props;
+
+    const {
+      isOpen,
+    } = this.state;
 
     const navbarClass = classNames({
       navbar: true,
       [`navbar-${colorScheme}`]: !!colorScheme,
       [className]: !!className,
     });
+    const collapserClass = classNames({
+      'navbar-collapse': true,
+      collapse: useToggler,
+      show: useToggler && isOpen,
+    });
+
+    if (useToggler) {
+      return (
+        <nav className={navbarClass} {...props}>
+          {!!brand && brand()}
+          {toggler({ onClick: this.handleTogglerClick, isOpen })}
+          <div className={collapserClass}>
+            {children}
+          </div>
+        </nav>
+      );
+    }
 
     return (
       <nav className={navbarClass} {...props}>
